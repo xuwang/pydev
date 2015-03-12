453035a39564fed0a207fda2c32fe1ccb26e8101
Xu Wang <xuwang@gmail.com>
2015-03-11 21:58:56 -0700

    add mongodb to the box

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -5,6 +5,15 @@ include system-update
 include python
 include apache
 
+class {'::mongodb::server':
+  auth => true,
+}
+
+mongodb::db { 'testdb':
+  user          => 'user1',
+  password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
+}
+
 python::pip { 'awscli' :
     pkgname       => 'awscli'
 }
